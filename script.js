2b1d6e15ebc6e27e0657cd2020bd1d43829a22eb
elliottdinley <elliott.dinley1@gmail.com>
2023-10-19 00:57:59 +0100

    completed

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Write your code here
+let lightbulbs = document.querySelectorAll("div .item");
+let output = document.querySelector("h3.subtitle");
+let count = 0;
+
+lightbulbs.forEach(function(lb) {
+    lb.addEventListener("click", function() {
+        lb.classList.toggle("active");
+        count ++
+        if (count === 1) {
+            output.innerHTML = "You've clicked the lights " + count + " time"
+        } else {
+            output.innerHTML = "You've clicked the lights " + count + " times"
+        }
+    });
+});
